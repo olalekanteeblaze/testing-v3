d2b7610887fc3ff5285cd9c204f7005aad28ed6f
olalekanteeblaze <olalekanteeblaze@gmail.com>
2022-08-26 15:04:27 +0100

    updated App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect, useState } from 'react';
 import { w3cwebsocket as W3CWebSocket } from "websocket";
 import Identicon from 'react-identicons';
 import {
@@ -14,84 +14,58 @@ import './App.css';
 const client = new W3CWebSocket('ws://127.0.0.1:8000');
 const contentDefaultMessage = "Start writing your document here";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      currentUsers: [],
-      userActivity: [],
-      username: null,
-      text: ''
-    };
-  }
+const App = () => {
+  const [user, setUser] = useState('')
+  const [username, setUsername] = useState('')
+  const [currentUsers, setCurrentUsers] = useState([])
+  const [text, setText] = useState('')
+  const [userActivity, setUserActivity] = useState([])
 
-  logInUser = () => {
-    const username = this.username.value;
+  const logInUser = () => {
+    const username = user
     if (username.trim()) {
-      const data = {
-        username
-      };
-      this.setState({
-        ...data
-      }, () => {
-        client.send(JSON.stringify({
-          ...data,
-          type: "userevent"
-        }));
-      });
+      setUsername(username)
     }
   }
 
-  /* When content changes, we send the
+    /* When content changes, we send the
 current content of the editor to the server. */
- onEditorStateChange = (text) => {
-   client.send(JSON.stringify({
-     type: "contentchange",
-     username: this.state.username,
-     content: text
-   }));
- };
+ const onEditorStateChange = (text) => {
+  client.send(JSON.stringify({
+    type: "contentchange",
+    username,
+    content: text
+  }));
+};
 
- componentWillMount() {
-   client.onopen = () => {
-     console.log('WebSocket Client Connected');
-   };
-   client.onmessage = (message) => {
-     const dataFromServer = JSON.parse(message.data);
-     const stateToChange = {};
-     if (dataFromServer.type === "userevent") {
-       stateToChange.currentUsers = Object.values(dataFromServer.data.users);
-     } else if (dataFromServer.type === "contentchange") {
-       stateToChange.text = dataFromServer.data.editorContent || contentDefaultMessage;
-     }
-     stateToChange.userActivity = dataFromServer.data.userActivity;
-     this.setState({
-       ...stateToChange
-     });
-   };
- }
+  useEffect(() => {
+    if(username) {
+      client.send(JSON.stringify({
+        username,
+        type: "userevent"
+      }))
+    }
+  },[username])
 
-  showLoginSection = () => (
-    <div className="account">
-      <div className="account__wrapper">
-        <div className="account__card">
-          <div className="account__profile">
-            <Identicon className="account__avatar" size={64} string="randomness" />
-            <p className="account__name">Hello, user!</p>
-            <p className="account__sub">Join to edit the document</p>
-          </div>
-          <input name="username" ref={(input) => { this.username = input; }} className="form-control" />
-          <button type="button" onClick={() => this.logInUser()} className="btn btn-primary account__btn">Join</button>
-        </div>
-      </div>
-    </div>
-  )
-
-  showEditorSection = () => (
+  useEffect(() => {
+    client.onopen = () => {
+      console.log('WebSocket Client Connected');
+    };
+    client.onmessage = (message) => {
+      const dataFromServer = JSON.parse(message.data);
+      if (dataFromServer.type === "userevent") {
+        setCurrentUsers(Object.values(dataFromServer.data.users));
+      } else if (dataFromServer.type === "contentchange") {
+        setText(dataFromServer.data.editorContent || contentDefaultMessage)
+      }
+      setUserActivity(dataFromServer.data.userActivity)
+    };
+  },[])
+  const showEditorSection = () => (
     <div className="main-content">
       <div className="document-holder">
         <div className="currentusers">
-          {this.state.currentUsers.map(user => (
+          {currentUsers.map(user => (
             <React.Fragment>
               <span id={user.username} className="userInfo" key={user.username}>
                 <Identicon className="account__avatar" style={{ backgroundColor: user.randomcolor }} size={40} string={user.username} />
@@ -105,37 +79,47 @@ current content of the editor to the server. */
         <Editor
           options={{
             placeholder: {
-              text: this.state.text ? contentDefaultMessage : ""
+              text: text ? contentDefaultMessage : ""
             }
           }}
           className="body-editor"
-          text={this.state.text}
-          onChange={this.onEditorStateChange}
+          text={text}
+          onChange={onEditorStateChange}
         />
       </div>
       <div className="history-holder">
         <ul>
-          {this.state.userActivity.map((activity, index) => <li key={`activity-${index}`}>{activity}</li>)}
+          {userActivity.map((activity, index) => <li key={`activity-${index}`}>{activity}</li>)}
         </ul>
       </div>
     </div>
   )
 
-  render() {
-    const {
-      username
-    } = this.state;
-    return (
-      <React.Fragment>
-        <Navbar color="light" light>
-          <NavbarBrand href="/">Real-time document editor</NavbarBrand>
-        </Navbar>
-        <div className="container-fluid">
-          {username ? this.showEditorSection() : this.showLoginSection()}
+  const showLoginSection = () => (
+    <div className="account">
+      <div className="account__wrapper">
+        <div className="account__card">
+          <div className="account__profile">
+            <Identicon className="account__avatar" size={64} string="randomness" />
+            <p className="account__name">Hello, user!</p>
+            <p className="account__sub">Join to edit the document</p>
+          </div>
+          <input name="username" onChange={(e) => setUser(e.target.value)} className="form-control" />
+          <button type="button" onClick={logInUser} className="btn btn-primary account__btn">Join</button>
         </div>
-      </React.Fragment>
-    );
-  }
+      </div>
+    </div>
+  )
+  return (
+    <React.Fragment>
+      <Navbar color="light" light>
+        <NavbarBrand href="/">Real-time document editor</NavbarBrand>
+      </Navbar>
+      <div className="container-fluid">
+        {username ? showEditorSection() : showLoginSection()}
+      </div>
+    </React.Fragment>
+  )
 }
 
 export default App;
